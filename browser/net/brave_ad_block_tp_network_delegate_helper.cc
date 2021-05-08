b8ff718c222642a1d6c9eb9b9da64488e960973d
Anton Lazarev <antonok35@gmail.com>
2021-05-07 23:20:51 -0700

    add mock host resolver to unit tests

--- browser/net/brave_ad_block_tp_network_delegate_helper.cc
+++ browser/net/brave_ad_block_tp_network_delegate_helper.cc
@@ -114,7 +114,6 @@ class AdblockCnameResolveHostClient : public network::mojom::ResolveHostClient {
       const ResponseCallback& next_callback,
       scoped_refptr<base::SequencedTaskRunner> task_runner,
       std::shared_ptr<BraveRequestInfo> ctx) {
-    DCHECK(ctx->browser_context);
     cb_ = base::BindOnce(&ShouldBlockAdWithOptionalCname, task_runner,
                          std::move(next_callback), ctx);
 
@@ -133,11 +132,6 @@ class AdblockCnameResolveHostClient : public network::mojom::ResolveHostClient {
     if (secure_dns_config.mode() == net::SecureDnsMode::kSecure)
       optional_parameters->source = net::HostResolverSource::DNS;
 
-    network::mojom::NetworkContext* network_context =
-        content::BrowserContext::GetDefaultStoragePartition(
-            ctx->browser_context)
-            ->GetNetworkContext();
-
     start_time_ = base::TimeTicks::Now();
 
     if (g_testing_host_resolver) {
@@ -145,6 +139,13 @@ class AdblockCnameResolveHostClient : public network::mojom::ResolveHostClient {
           net::HostPortPair::FromURL(ctx->request_url), network_isolation_key,
           std::move(optional_parameters), receiver_.BindNewPipeAndPassRemote());
     } else {
+      DCHECK(ctx->browser_context);
+
+      network::mojom::NetworkContext* network_context =
+          content::BrowserContext::GetDefaultStoragePartition(
+              ctx->browser_context)
+              ->GetNetworkContext();
+
       network_context->ResolveHost(
           net::HostPortPair::FromURL(ctx->request_url), network_isolation_key,
           std::move(optional_parameters), receiver_.BindNewPipeAndPassRemote());
